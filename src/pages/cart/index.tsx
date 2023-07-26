40a60aa97e7061d8340a5a3dc275a6bd269fae54
Daniel Felix <felixdaniel9@gmail.com>
2023-07-26 16:10:15 -0300

    Initial commit creating project

---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,8 @@
+
+export function Cart(){
+  return(
+    <div>
+      <h1>CART Page</h1>
+    </div>
+  )
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+
+export function Home(){
+  return(
+    <div>
+      <h1>HOME page</h1>
+    </div>
+  )
+}
