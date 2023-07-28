f4e736f3f869880f3ca97cb139693654da0abb82
Daniel Felix <felixdaniel9@gmail.com>
2023-07-28 10:52:45 -0300

    Add item to cart

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,8 +1,10 @@
 import { createContext, ReactNode, useState } from "react";
+import { ProductProps } from "../pages/home";
 
 interface CartContextData{
   cart: CartProps[];
   cartAmount: number;
+  addItemCart: (newItem: ProductProps) => void;
 }
 
 interface CartProps{
@@ -25,10 +27,35 @@ export const CartContext = createContext({} as CartContextData);
 function CartProvider({children}: CartProviderProps){
   const [cart, setCart] = useState<CartProps[]>([]);
 
+  // Função de adicionar item no cart.
+  function addItemCart(newItem: ProductProps){
+    //Se já existe um item igual no cart.
+    const indexItem = cart.findIndex(item => item.id === newItem.id); // Se não existe devolve -1
+
+    if(indexItem !== -1){
+      // Se entrou aqui apenas somamos +1 na quantidade e calculamos o total do cart.
+      let cartList = cart;
+
+      cartList[indexItem].amount = cartList[indexItem].amount +1;
+      cartList[indexItem].total = cartList[indexItem].amount * cartList[indexItem].price;
+
+      return;
+    }
+    // Adicionar um item diferente no cart.
+    let data = {
+      ...newItem,
+      amount: 1,
+      total: newItem.price
+    }
+
+    setCart(products => [...products, data]);
+  }
+
   return(
     <CartContext.Provider value={{
       cart,
-      cartAmount: cart.length  // Se refere a quantidade que aparece na bolinha do cart
+      cartAmount: cart.length,  // Se refere a quantidade que aparece na bolinha do cart
+      addItemCart,
     }}>
       {children}
     </CartContext.Provider>
