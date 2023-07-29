aad40de4a996dda2b0f76dd34b842807f528ef4e
Daniel Felix <felixdaniel9@gmail.com>
2023-07-29 15:36:40 -0300

    Creating remove and total function

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -5,6 +5,8 @@ interface CartContextData{
   cart: CartProps[];
   cartAmount: number;
   addItemCart: (newItem: ProductProps) => void;
+  removeItemCart: (product: CartProps) => void;
+  total: string;
 }
 
 interface CartProps{
@@ -26,8 +28,8 @@ export const CartContext = createContext({} as CartContextData);
 // Aqui o provider
 function CartProvider({children}: CartProviderProps){
   const [cart, setCart] = useState<CartProps[]>([]);
+  const [total, setTotal] =useState('');
 
-  // Função de adicionar item no cart.
   function addItemCart(newItem: ProductProps){
     //Se já existe um item igual no cart.
     const indexItem = cart.findIndex(item => item.id === newItem.id); // Se não existe devolve -1
@@ -39,6 +41,7 @@ function CartProvider({children}: CartProviderProps){
       cartList[indexItem].amount = cartList[indexItem].amount +1;
       cartList[indexItem].total = cartList[indexItem].amount * cartList[indexItem].price;
 
+      totalResultCart(cartList);
       return;
     }
     // Adicionar um item diferente no cart.
@@ -49,6 +52,36 @@ function CartProvider({children}: CartProviderProps){
     }
 
     setCart(products => [...products, data]);
+    totalResultCart([...cart, data]);
+  }
+
+  function removeItemCart(product: CartProps){
+    // Se o produto é igual o que está no cart
+    const indexItem = cart.findIndex(item => item.id === product.id);
+
+    if(cart[indexItem]?.amount > 1){
+      // Diminuir apenas 1 amount do que vc tem
+      let cartList = cart;
+
+      cartList[indexItem].amount = cartList[indexItem].amount -1;
+      cartList[indexItem].total = cartList[indexItem].total - cartList[indexItem].price;
+
+      setCart(cartList);
+      totalResultCart(cartList);
+      return;
+    }
+
+    // Se tiver apenas 1 amount, fazer a remoção.
+    const removeItem = cart.filter(item => item.id !== product.id);
+    setCart(removeItem);
+    totalResultCart(removeItem);
+  }
+
+  function totalResultCart(items: CartProps[]){
+    let myCart = items;
+    let result = myCart.reduce((acc, obj) => {return acc + obj.total}, 0);
+    const resultFormated = result.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    setTotal(resultFormated);
   }
 
   return(
@@ -56,6 +89,8 @@ function CartProvider({children}: CartProviderProps){
       cart,
       cartAmount: cart.length,  // Se refere a quantidade que aparece na bolinha do cart
       addItemCart,
+      removeItemCart,
+      total,
     }}>
       {children}
     </CartContext.Provider>
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -4,7 +4,7 @@ import { CartContext } from '../../contexts/CartContext';
 import { Link } from 'react-router-dom';
 
 export function Cart(){
-  const {cart} = useContext(CartContext);
+  const {cart, total, addItemCart, removeItemCart} = useContext(CartContext);
 
   return(
     <div className="w-full max-w-7xl px-4 mx-auto">
@@ -35,11 +35,17 @@ export function Cart(){
           })}</strong>
   
           <div className="flex items-center justify-center gap-3">
-            <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            <button
+              onClick={() => removeItemCart(item)} 
+              className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center"
+            >
               -
             </button>
               {item.amount}
-            <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            <button 
+              onClick={() => addItemCart(item)}
+              className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center"
+            >
               +
             </button>
           </div>
@@ -49,7 +55,7 @@ export function Cart(){
           })}</strong>
         </section>
       ))}
-      {cart.length !== 0 && <p className="font-bold mt-4">Total: R$ 1.000</p>}
+      {cart.length !== 0 && <p className="font-bold mt-4">Total: {total}</p>}
     </div>
   )
 }
